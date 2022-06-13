48ef30dcaa4a6e5cc134b0c6f252fea752ee4f8e
JunyaSugita <k021g1293@g.neec.ac.jp>
2022-06-13 14:54:52 +0900

    途中

--- Player.cpp
+++ Player.cpp
@@ -18,7 +18,23 @@ void Player::Initialize(Model* model, uint32_t textureHandle){
 void Player::Update(){
 	Vector3 move = { 0,0,0 };
 
-	if(input_->)
+	if (input_->PushKey(DIK_W)) {
+		move.z += 1.0f;
+	}
+	if (input_->PushKey(DIK_S)) {
+		move.z -= 1.0f;
+	}
+	if (input_->PushKey(DIK_A)) {
+		move.x -= 1.0f;
+	}
+	if (input_->PushKey(DIK_D)) {
+		move.x += 1.0f;
+	}
+	MatCalc(worldTransform_);
+
+	debugText_->SetPos(0, 0);
+	debugText_->Printf("player = %f,%f,%f", worldTransform_.translation_.x, worldTransform_.translation_.y, worldTransform_.translation_.z);
+
 }
 
 void Player::Draw(){}
